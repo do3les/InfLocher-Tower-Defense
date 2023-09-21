e5ee4f016a2920ace36455c3934173c261713ead
Physalis <143352299+Physalis2@users.noreply.github.com>
2023-09-22 00:07:06 +0200

    Add files via upload

--- Turrets/TurretScript.gd
+++ Turrets/TurretScript.gd
@@ -1,24 +1,40 @@
 extends Node
 
-var enemy_array = []
+var enemyArray = []
+var enemy
 var built = false
 
 func _physics_process(delta):
-	turn()
+	if enemyArray.size() != 0: #and built:
+		selectEnemy()
+		turn()
+	else:
+		enemy = null
 
-func _ready():
-	if built:
-		self.get_node("Range/CollisionShape2D").get_shape().radius = GameData.towerStats[self.get_name()]["range"]
+#func _ready():
+#	if built:
+#		self.get_node("Range/CollisionShape2D").get_shape().radius = GameData.towerStats[self.get_name()]["range"]
+
+func selectEnemy():
+	var progressionEnemyAr = []
+	for i in progressionEnemyAr:
+		progressionEnemyAr.append(i.offset)
+	var furthestEnemy = progressionEnemyAr.max()
+	var targetEnemy = progressionEnemyAr.find(furthestEnemy)
+	enemy = enemyArray[targetEnemy]
 
 
 func turn():
-	var enemyPos
+	get_node("TowerHead").look_at(enemy.position)
+	
+
+
 
 
 func _on_range_body_entered(body):
-	enemy_array.append(body.get_parent())
-	print(enemy_array)
+	enemyArray.append(body.get_parent())
+	print(enemyArray)
 
 
 func _on_range_body_exited(body):
-	enemy_array.append(body.get_parent())
+	enemyArray.append(body.get_parent())
